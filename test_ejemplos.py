83607f20e2d80fbe3b23e9bf6877025eea22a21f
Jeremias Lavielle <laviellejeremias27@gmail.com>
2026-06-14 21:34:28 -0300

    agrego test

---
+++ test_ejemplos.py
@@ -0,0 +1,18 @@
+import pytest
+from main import lexer_multiples_afds
+
+archivos = [
+    "Codigos_ejemplo/ejemplo1.txt",
+    "Codigos_ejemplo/ejemplo2.txt",
+    "Codigos_ejemplo/ejemplo3.txt",
+]
+
+@pytest.mark.parametrize("archivo", archivos)
+
+def test_ejemplo(archivo):
+    fuente = open(archivo, encoding="utf-8").read()
+    tokens = lexer_multiples_afds(fuente)   # si el código no es válido, lanza error y falla
+    print(f"\n--- {archivo} ---")
+    for t in tokens:
+        print("   ", t)
+    assert tokens[-1] == ("EOF", "EOF")     # lo aceptó hasta el final
